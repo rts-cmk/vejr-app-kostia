86bceb2b9314ee864058954ab223f39e9bf560e7
Overlordka <kopary787@gmail.com>
2025-09-30 08:26:06 +0200

    AppJSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,32 @@ import { useState, useEffect } from "react"
 import WeatherCard from "./components/WeatherCard"
 import SearchBar from "./components/SearchBar"
 
-function App() {
+const API_KEY = "a4c1a999a5f59b1ae04c5fba9554baab"
+
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+export default function App() {
+  const [city, setCity] = useState("");
+  const [searchCity, setSearchCity] = useState("Roskilde");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (!searchCity) return;
+
+    fetch(`${BASE_URL}?q=${searchCity}&appid=${API_KEY}`)
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, [searchCity]);
+
+  function handleSearch(e) {
+    e.preventDefault();
+    setSearchCity(city);
+  }
 
   return (
-    <>
-    </>
-  )
+    <section>
+      <SearchBar city={city} setCity={setCity} onSearch={handleSearch} />
+      <WeatherCard data={data} />
+    </section>
+  );
 }
-
-export default App
